6a46187b8faa8013d7e2567cf3cf41718eb16e14
Krisha Raj <krisha.raj@gmail.com>
2026-05-11 20:22:44 +1000

    Create basic webpages

--- main.py
+++ main.py
@@ -5,4 +5,18 @@ app.secret_key = "APP_SECRET_KEY"
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html"  )
+  
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+
+if __name__=="__main__":
+    app.run(debug=False)
+    
